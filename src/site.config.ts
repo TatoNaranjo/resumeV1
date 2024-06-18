ecd4f3b19eb64f17446d504021d419255edb9028
TatoNaranjo <naranjosa2004@gmail.com>
2024-06-17 22:36:26 -0500

    Remove unused code and update author and title in site configuration

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,18 +3,18 @@ import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'SRLEOM',
+	author: 'Camilo Reyes',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'astro-theme-resume',
+	title: 'Camilo Reyes - Portfolio',
 	// Meta property used as the default description meta property
-	description: 'The official Astro Resume Theme',
+	description: 'The official Portfolio used by Camilo Reyes to showcase his work.',
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: 'en-GB',
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: 'en_GB',
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: 'en-GB',
+		locale: 'es-CO',
 		options: {
 			day: 'numeric',
 			month: 'short',
@@ -28,10 +28,6 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 		title: 'Home',
 		path: '/'
 	},
-	{
-		title: 'Blog',
-		path: '/blog/'
-	}
 ]
 
 // https://expressive-code.com/reference/configuration/
